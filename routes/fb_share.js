550eee22342f12cfd6d87dd9698eaa5eafb99763
meowtsai <shihfan.tsai@gmail.com>
2020-12-03 11:12:45 +0800

    try pug engine

--- routes/fb_share.js
+++ routes/fb_share.js
@@ -6,7 +6,10 @@ const EventUser = require("../models/EventUser");
 const appConfig = require("../config/cosplay");
 
 router.get("/", async (req, res) => {
-  res.render("../pugview/fblink", { title: "Hey", message: "Hello there!" });
+  res.render("../pugview/fblink", {
+    title: "棋佈星羅",
+    message: "第五人格監管者 雕刻家 伽拉泰亞 金皮!",
+  });
 });
 
 module.exports = router;
